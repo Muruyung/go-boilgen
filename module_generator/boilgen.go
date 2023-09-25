91761c443a013bcfa336542d51244bbb1421328e
Robi Naufal Kaosar <Robi.kaosar@salt.co.id>
2023-09-25 15:49:03 +0700

    refactor: update version

--- module_generator/boilgen.go
+++ module_generator/boilgen.go
@@ -14,7 +14,7 @@ var (
 		Short:   "Generate core modules with fields",
 		Long:    `This subcommand used to creating core modules (usecase, service, repository, entity)`,
 		Run:     modGen,
-		Version: "1.3.1",
+		Version: "1.4.0",
 	}
 )
 
