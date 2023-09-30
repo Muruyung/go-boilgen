ff0fe6e912fb3e28225b99a6c76a7c30b8e28966
Robi Naufal Kaosar <Robi.kaosar@salt.co.id>
2023-09-30 21:43:13 +0700

    fix: change version cobra command

--- module_generator/boilgen.go
+++ module_generator/boilgen.go
@@ -14,7 +14,7 @@ var (
 		Short:   "Generate core modules with fields",
 		Long:    `This subcommand used to creating core modules (usecase, service, repository, entity)`,
 		Run:     modGen,
-		Version: "2.0.0",
+		Version: "1.5.0",
 	}
 )
 
